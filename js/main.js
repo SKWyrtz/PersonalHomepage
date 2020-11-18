2d5742c7e23d009fb0148ca280f70f77ce0aa623
Sigurd W <au648909@uni.au.dk>
2020-11-18 17:30:31 +0100

    Introduced time and date display

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 let csgo1 = document.querySelector(".csgo")
+let time = document.querySelector("#time")
+let date = document.querySelector("#date")
 
 let result
 
-test()
+fetchCSGO()
+setTime()
 
 
-async function test(){
+function setTime(){
+  let today = new Date
+  console.log(today)
+
+  let currentTime = today.getHours() + ":" + today.getMinutes()
+  let currentDate = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear()
+  time.innerHTML = currentTime
+  date.innerHTML = currentDate
+}
+
+async function fetchCSGO(){
     await fetch('https://www.reddit.com/r/GlobalOffensive/top.json?limit=10')
   .then(response => response.json())
   .then(data => result = data);
-  console.table(result)
+  console.table(result.data.children[0].data)
   csgo1.innerHTML = result.data.children[0].data.title
+
   //console.table(data.data.children[0].data.title)
 /*
   axios.get('https://www.reddit.com/r/GlobalOffensive/top.json?limit=10').then(response => {
